0e3a5810b3471c3c1d1ea822f06064dabeb3000e
Benjamin Davis <benjamin.j.davis96@gmail.com>
2022-04-12 17:39:21 -0700

    moved project guide PDF up a level

--- Projects/project_guide/project_guide.mk
+++ Projects/project_guide/project_guide.mk
@@ -5,11 +5,11 @@
 #   This file contains the build script for the project guide PDF
 #####################################################################
 
-PROJECT_GUIDE_PDFS ?= Project_Guide.pdf
+PROJECT_GUIDE_PDFS ?= ../Project_Guide.pdf
 
 all : ${PROJECT_GUIDE_PDFS}
 
-%.pdf : src/%.tex src/*.tex
+../%.pdf : src/%.tex src/*.tex
 	@mkdir -p build
 	cd build; latexmk -silent -pdf ../$< > /dev/null
 	@mv build/$*.pdf $@
